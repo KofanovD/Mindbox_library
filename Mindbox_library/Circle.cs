6c9880e3c150ddac0a77f545bd3b27da0a845b9b
KofanovD <shad-xx@yandex.ru>
2023-11-19 11:23:10 +0700

    added a circle negative radius exception

--- Mindbox_library/Circle.cs
+++ Mindbox_library/Circle.cs
@@ -11,6 +11,10 @@ namespace Mindbox_library
         private double _radius { get; set; }
         public Circle(double radius)
         {
+            if (radius <= 0)
+            {
+                throw new ArgumentException("Радиус должен быть больше 0!");
+            }
             _radius = radius;
         }
 
--- Mindbox_library_test/UnitTest1.cs
+++ Mindbox_library_test/UnitTest1.cs
@@ -17,6 +17,17 @@ namespace Mindbox_library_test
             double expectedArea = Math.PI * radius * radius;
             Assert.That(circle.GetArea(), Is.EqualTo(expectedArea).Within(0.001));
         }
+
+        [Test]
+        public void TestCircleNegativeRadius()
+        {
+            double radius = -5;
+
+            // Проверка на выброс исключения
+            var ex = Assert.Throws<ArgumentException>(() => new Circle(radius));
+            Assert.That(ex.Message, Is.EqualTo("Радиус должен быть больше 0!"));
+        }
+
         [Test]
         public void TestTriangleArea()
         {
